c45a58289d6e5953e174aba3f1af7ec35ea4d02e
misupopo <kokorononakaniitumo@yahoo.co.jp>
2021-12-24 16:54:42 +0900

    add initi.d shell

--- init-dump/dump.sh
+++ init-dump/dump.sh
@@ -2,4 +2,3 @@
 
 mysql -uexample -pexample1234 -e "CREATE DATABASE IF NOT EXISTS example default character set utf8 ;"
 mysql -uexample -pexample1234 example < /schema/example-product-master-sql-basic-data.sql
-echo "test"
